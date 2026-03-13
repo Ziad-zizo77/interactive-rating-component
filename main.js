0a7fc5d4c40ca1e0fc88f336a4a94c21f56ea7dc
unknown <zm4025828@gmail.com>
2026-03-13 16:26:32 +0200

    interactive rating component

---
+++ main.js
@@ -0,0 +1,38 @@
+const nums = document.querySelectorAll(".num");
+const btn = document.querySelector(".btn");
+
+const ratingCard = document.querySelector(".rating-card");
+const thankCard = document.querySelector(".thank-card");
+
+const ratingText = document.querySelector(".text");
+const backBtn = document.querySelector(".back-btn");
+
+let selectedRating = 4;
+
+// selectedRating
+nums.forEach((num) => {
+  num.addEventListener("click", () => {
+    nums.forEach((n) => n.classList.remove("active"));
+
+    num.classList.add("active");
+
+    selectedRating = num.textContent;
+  });
+});
+
+// submit
+btn.addEventListener("click", () => {
+
+  ratingCard.classList.add("hidden");
+
+  ratingText.textContent = `You selected ${selectedRating} out of 5`;
+
+  thankCard.classList.remove("hidden");
+});
+
+// back button
+backBtn.addEventListener("click", () => {
+  thankCard.classList.add("hidden");
+
+  ratingCard.classList.remove("hidden");
+});
